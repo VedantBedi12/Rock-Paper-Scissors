de4254b9a4afe1e62b276244b84e01d9e440629d
Vedant Bedi <143510228+VedantBedi12@users.noreply.github.com>
2024-03-15 16:59:57 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,23 +1,27 @@
 let userscore = 0;
 let compscore = 0;
-const choicesa = document.querySelectorAll(".choices");
+const choicesa = document.querySelectorAll(".choice");
+choicesa.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const id = choice.querySelector("p").innerText.toLowerCase();
+        console.log(id);
+        playgame(id);
+    });
+});
+
 
 const gencompchoice = () => {
     const options = ["rock", "scissors", "paper"];
     let ind = Math.floor(Math.random() * 3);
     return options[ind];
 }
-
 const playgame = (id) => {
     const compchoice = gencompchoice();
-    console.log(compchoice);
     const userchoice = id;
-    
     if (id === compchoice) {
         document.getElementById("result").innerText = "Draw";
     } else {
         let userwin = false;
-        
         if ((userchoice === "rock" && compchoice === "scissors") ||
             (userchoice === "scissors" && compchoice === "paper") ||
             (userchoice === "paper" && compchoice === "rock")) {
@@ -33,11 +37,3 @@ const playgame = (id) => {
         }
     }
 }
-
-choicesa.forEach((choice) => {
-    choice.addEventListener("click", () => {
-        const id = choice.querySelector("p").innerText.toLowerCase();
-        console.log(id);
-        playgame(id);
-    });
-});
